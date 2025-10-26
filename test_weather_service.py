dcaa98d9c2ed1f1bdd65cce9ecac6bbae88d7f4c
zxhom26 <125313839+zxhom26@users.noreply.github.com>
2025-10-26 14:44:14 -0700

    making string

--- test_weather_service.py
+++ test_weather_service.py
@@ -22,7 +22,7 @@ class WeatherServiceTest(unittest.TestCase):
         self.assertEqual(isHot2, False)
 
     def test_get_greeting(self): 
-        with patch(weather_service.datetime) as mock_datetime:
+        with patch('weather_service.datetime') as mock_datetime:
             mock_datetime.now.return_value = datetime(2025, 1, 1, 14, 0, 0)
             greeting = self.ws.get_greeting('Zach')
             self.assertEqual(greeting, "Good afternoon, Zach!")
